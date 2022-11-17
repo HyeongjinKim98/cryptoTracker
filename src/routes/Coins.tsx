312cd314c17b516b48ec59133ee6c931e8c24766
HyeongjinKim98 <201721157@sangmyung.kr>
2022-11-17 16:10:38 +0900

    update router ver6

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -85,7 +85,7 @@ function Coins() {
                                 <Link
                                     to={{
                                         pathname : `/${coin.id}`,
-                                        state : {name : coin.name}
+                                        // state : {name : coin.name}
                                 }}>
                                     <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} />
                                     {coin.name} &rarr;
