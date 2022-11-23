ff962e1a4796aadc9faada8af4921970976bc35a
HyeongjinKim98 <201721157@sangmyung.kr>
2022-11-23 15:56:57 +0900

    apply nested routes v6_ 221123

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { useLocation } from "react-router-dom";
+import { Outlet,Route,Routes,useLocation } from "react-router-dom";
 import styled from "styled-components";
+import Chart from "./Chart";
+import Price from "./Price";
 
 const Loader = styled.span`
     text-align : center;
@@ -24,12 +26,37 @@ const Header = styled.header`
 
 `;
 
-interface RouteParams {
-    coinId: string;
-}
-interface RouteState{
-    name : string;
-}
+const Overview = styled.div`
+    display: flex;
+    justify-content : space-between;
+    background-color: rgba(0,0,0,0.5);
+    padding : 10px 20px;
+    border-radius : 10px;
+`;
+
+const OverviewItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
+    span:first-child{
+        font-size: 10px;
+        font-weight: 400;
+        text-transform: uppercase;
+        margin-bottom : 5px;
+    }
+`;
+
+const Description = styled.p`
+    margin : 20px 0px;
+`;
+// Old Version (before react-dom 6.0)
+// interface RouteParams {
+//     coinId: string;
+// }
+// interface RouteState{
+//     name : string;
+// }
 // interface ITag{
 //     coin_counter : number;
 //     ico_counter : number;
@@ -103,22 +130,63 @@ function Coin() {
             const infoData = await(
                 await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
             ).json();
-            console.log(infoData)
             const priceData = await(
                 await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
             ).json();
-            console.log(priceData)
             setInfo(infoData);
             setPriceInfo(priceData);
+            setLoading(false);
         })();
-    },[]);
+    },[coinId]);
     return (
         <Container>
             <Header>
-                <Title>{state?.name||"Loading"}</Title>
+                <Title>{state?.name ? state.name : loading ? "Loading...": info?.name}</Title>
             </Header>
-            {loading ? (<Loader>Loading...</Loader>) : null}
+            {loading ? (
+                <Loader>Loading...</Loader>
+            ) : (
+                <>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Rank:</span>
+                            <span>{info?.rank}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Symbol:</span>
+                            <span>${info?.symbol}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Open Source:</span>
+                            <span>{info?.open_source ? "Yes" : "No"}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Description>{info?.description}</Description>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Total Suply:</span>
+                            <span>{priceInfo?.total_supply}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Max Supply:</span>
+                            <span>{priceInfo?.max_supply}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Routes>
+                            <Route path="price" element={<Price />} />
+                            <Route path="chart" element={<Chart />} />
+                        {/* it's old */}
+                        {/* <Route path={`/${coinId}/price`}>
+                            <Price />
+                        </Route>
+                        <Route path={`/${coinId}/chart`}>
+                            <Chart />
+                        </Route> */}
+                    </Routes>
+                </>
+            )}
         </Container>
+        
     );
 }
 export default Coin;
